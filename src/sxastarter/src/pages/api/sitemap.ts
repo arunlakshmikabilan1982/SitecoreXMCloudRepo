9601da2272ddbeafc7b67a616fe29ac2865ad8dc
Karthick dp <39872973+dpkarthick@users.noreply.github.com>
2022-12-01 14:30:40 +0530

    Initial commit

---
+++ src/sxastarter/src/pages/api/sitemap.ts
@@ -0,0 +1,70 @@
+import { AxiosResponse } from 'axios';
+import type { NextApiRequest, NextApiResponse } from 'next';
+import config from 'temp/config';
+import { AxiosDataFetcher, GraphQLSitemapXmlService } from '@sitecore-jss/sitecore-jss-nextjs';
+
+const ABSOLUTE_URL_REGEXP = '^(?:[a-z]+:)?//';
+
+const sitemapApi = async (
+  req: NextApiRequest,
+  res: NextApiResponse
+): Promise<NextApiResponse | void> => {
+  const {
+    query: { id },
+  } = req;
+  // create sitemap graphql service
+  const sitemapXmlService = new GraphQLSitemapXmlService({
+    endpoint: config.graphQLEndpoint,
+    apiKey: config.sitecoreApiKey,
+    siteName: config.jssAppName,
+  });
+
+  // if url has sitemap-{n}.xml type
+  if (id) {
+    const sitemapPath = await sitemapXmlService.getSitemap(id as string);
+
+    // if sitemap is match otherwise redirect to 404 page
+    if (sitemapPath) {
+      const isAbsoluteUrl = sitemapPath.match(ABSOLUTE_URL_REGEXP);
+      const sitemapUrl = isAbsoluteUrl ? sitemapPath : `${config.sitecoreApiHost}${sitemapPath}`;
+      res.setHeader('Content-Type', 'text/xml;charset=utf-8');
+
+      // need to prepare stream from sitemap url
+      return new AxiosDataFetcher()
+        .get(sitemapUrl, {
+          responseType: 'stream',
+        })
+        .then((response: AxiosResponse) => {
+          response.data.pipe(res);
+        })
+        .catch(() => res.redirect('/404'));
+    }
+
+    return res.redirect('/404');
+  }
+
+  // this approache if user go to /sitemap.xml - under it generate xml page with list of sitemaps
+  const sitemaps = await sitemapXmlService.fetchSitemaps();
+
+  if (!sitemaps.length) {
+    return res.redirect('/404');
+  }
+
+  const SitemapLinks = sitemaps
+    .map(
+      (item) =>
+        `<sitemap>
+            <loc>${item}</loc>
+          </sitemap>
+        `
+    )
+    .join('');
+
+  res.setHeader('Content-Type', 'text/xml;charset=utf-8');
+
+  return res.send(`
+  <sitemapindex xmlns="http://sitemaps.org/schemas/sitemap/0.9">${SitemapLinks}</sitemapindex>
+  `);
+};
+
+export default sitemapApi;
