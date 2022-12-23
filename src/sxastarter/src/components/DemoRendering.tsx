d0225c679cc2c70ce02b1754b896cf3546f5d73a
Parvathi Somanahalli <parvathi.somanahalli@concentrix.com>
2022-12-23 18:48:55 +0530

    Made Changes

--- src/sxastarter/src/components/DemoRendering.tsx
+++ src/sxastarter/src/components/DemoRendering.tsx
@@ -2,22 +2,20 @@ import React from 'react';
 import { Field, Text } from '@sitecore-jss/sitecore-jss-nextjs';
 
 interface Fields {
-  SomeText: Field<string>;
+    SomeText: Field<string>;
 }
 
 export type DemoRenderingProps = {
-  params: { [key: string]: string };
-  fields: Fields;
+    params: { [key: string]: string };
+    fields: Fields;
 };
 
 export const Default = (props: DemoRenderingProps): JSX.Element => {
-  return (
-    <div className={`component demorendering ${props.params.styles}`}>
-      <div className="component-content">
-    <div>
-          <Text field={props.fields.SomeText} />
-    </div>
-    </div>
-    </div>
+    return (
+        <div className={`component·demorendering·${props.params.styles}`}
+        >
+            <div className="component-content">
+                <Text field={props.fields.SomeText}/></div>
+        </div>
     );
 };
