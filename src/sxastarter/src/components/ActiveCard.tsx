ee42a1bde057386690ccc04a029b9d617ae615bb
Rohit Goyal <rohit.goyal1@concentrix.com>
2023-07-27 13:49:38 +0530

    Fixed Lint Error

--- src/sxastarter/src/components/ActiveCard.tsx
+++ src/sxastarter/src/components/ActiveCard.tsx
@@ -108,16 +108,24 @@ export const ImageRight = (props: ActiveCardProps): JSX.Element => {
           <div className="float-start h-auto position-absolute promo-text top-50 w-50">
             <div className="text-center">
               <div className="field-promotext">
-                <JssRichText className="anchor-font fs-1 pb-1" field={props.fields.ActiveCardHeading} />
+                <JssRichText
+                  className="anchor-font fs-1 pb-1"
+                  field={props.fields.ActiveCardHeading}
+                />
               </div>
               <div className="field-promotext">
-                <JssRichText className="pb-5 pt-2 subheading-font" field={props.fields.ActiveCardDesc} />
+                <JssRichText
+                  className="pb-5 pt-2 subheading-font"
+                  field={props.fields.ActiveCardDesc}
+                />
               </div>
               <div className="bg-black field-promolink me-5 ms-5 p-4">
-                <JssLink className="p-4 text-decoration-none subheading-font text-white" field={props.fields.ActiveCardLink} />
+                <JssLink
+                  className="p-4 text-decoration-none subheading-font text-white"
+                  field={props.fields.ActiveCardLink}
+                />
               </div>
             </div>
-            
           </div>
         </div>
       </div>
