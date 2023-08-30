5747e96e7d072df6acce22f8e758bc43b2a07b87
parvathisomanahallicnx <parvathi.somanahalli@concentrix.com>
2023-08-30 13:07:41 +0530

    web personalization check in
    web personalization check in

--- src/sxastarter/src/components/CdpPageView.tsx
+++ src/sxastarter/src/components/CdpPageView.tsx
@@ -39,8 +39,8 @@ const CdpPageView = (): JSX.Element => {
       cookieDomain: window.location.host.replace(/^www\./, ''),
       // Cookie may be created in personalize middleware (server), but if not we should create it here
       forceServerCookieMode: false,
+      webPersonalization: true,
     });
-
     console.log('createpageView method in CDPpageView', engage);
     engage.pageView({
       channel: 'WEB',
