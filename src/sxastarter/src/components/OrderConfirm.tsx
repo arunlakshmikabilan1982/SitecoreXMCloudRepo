53c43a53b9d53b53cf12069a6b4a45f71baa7443
Amruta-gif <139867083+Amruta-gif@users.noreply.github.com>
2023-09-13 18:13:43 +0530

    CDP Flow

--- src/sxastarter/src/components/OrderConfirm.tsx
+++ src/sxastarter/src/components/OrderConfirm.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import Link from 'next/link';
 
+
+
 const Default = (): JSX.Element => (
-  <div className="component-content">
-    <span className="is-empty-hint">Order Confirmed</span>
-    <Link href="/">Continue Shopping</Link>
-  </div>
+    <div className="component-content">
+      <h1>Order has been succesful placed</h1>
+       <Link href="/">Continue Shopping</Link>
+    </div>
 );
-export { Default };
+export {Default};
+
