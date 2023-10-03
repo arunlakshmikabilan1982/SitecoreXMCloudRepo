0b93c2142ad62b1106bce5e1f71076c3818be212
Manish Yadav <manish.yadav7@concentrix.com>
2023-10-03 14:20:31 +0530

    mall enquiry form changes

---
+++ src/sxastarter/src/pages/api/mallenquiry.ts
@@ -0,0 +1,33 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  if (req.method !== 'POST') {
+    return res.status(405).json({ message: 'Method not allowed' });
+  }
+
+  try {
+    const user = JSON.parse(req.body);
+    console.log('mallenquiry.ts:' + JSON.stringify(user));
+
+    const createUser = await prisma.mallEnquiry.create({
+      data: {
+        FirstName: user.firstName,
+        LastName: user.lastName,
+        EnquiryType: user.enquiryType,
+        EnquiryDetails: user.enquiryDetails,
+        CreatedAt: new Date(Date.now()).toISOString(),
+        Email: user.email,
+      },
+    });
+
+    console.log('mallenquiry.ts: Enquiry created:', createUser);
+
+    res.status(200).json('success');
+  } catch (err) {
+    console.error('mallenquiry.ts: Error creating enquiry:', err);
+    res.status(400).json({ message: 'Something went wrong' });
+  }
+};
