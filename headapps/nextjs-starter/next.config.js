cc2abcfc5ea269c83f4cb8fe0b8e626fe985f797
parvathisomanahallicnx <parvathi.somanahalli@concentrix.com>
2025-02-17 15:16:16 +0530

    header change
    header change

--- headapps/nextjs-starter/next.config.js
+++ headapps/nextjs-starter/next.config.js
@@ -81,6 +81,14 @@ const nextConfig = {
       },
     ];
   },
+  async headers() {
+    return [
+     {
+        source: '/_next/:path*',
+        headers: [{ key: 'Access-Control-Allow-Origin', value: '*' }],
+      },
+    ];
+  },
 };
 
 module.exports = () => {
